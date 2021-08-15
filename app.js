f8c5348badaaa08db3f70c03ac2ed3f723c165ed
Akshathak97 <akshathak97@gmail.com>
2021-08-15 10:40:58 -0400

    updating js file

---
+++ app.js
@@ -0,0 +1,58 @@
+document.getElementById("demo1").onmouseover = function() {mouseOver()};
+document.getElementById("demo1").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("demo1").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById("demo1").style.color = "white";
+}
+//2
+document.getElementById("demo2").onmouseover = function() {mouseOver2()};
+document.getElementById("demo2").onmouseout = function() {mouseOut2()};
+
+function mouseOver2() {
+  document.getElementById("demo2").style.color = "red";
+}
+
+function mouseOut2() {
+  document.getElementById("demo2").style.color = "white";
+}
+
+
+//3
+document.getElementById("demo3").onmouseover = function() {mouseOver3()};
+document.getElementById("demo3").onmouseout = function() {mouseOut3()};
+
+function mouseOver3() {
+  document.getElementById("demo3").style.color = "red";
+}
+
+function mouseOut3() {
+  document.getElementById("demo3").style.color = "white";
+}
+
+//4
+document.getElementById("demo4").onmouseover = function() {mouseOver4()};
+document.getElementById("demo4").onmouseout = function() {mouseOut4()};
+
+function mouseOver4() {
+  document.getElementById("demo4").style.color = "red";
+}
+
+function mouseOut4() {
+  document.getElementById("demo4").style.color = "white";
+}
+
+//5
+document.getElementById("demo5").onmouseover = function() {mouseOver5()};
+document.getElementById("demo5").onmouseout = function() {mouseOut5()};
+
+function mouseOver5() {
+  document.getElementById("demo5").style.color = "red";
+}
+
+function mouseOut5() {
+  document.getElementById("demo5").style.color = "white";
+}
